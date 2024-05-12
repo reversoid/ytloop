c40ea4dd2cb9e432a64472c652b921ee37502390
reversoid <ancient.gekob@gmail.com>
2024-05-12 12:35:45 +0300

    Implement basis logic

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,7 +1,43 @@
+"use client";
+import { useRef, useState } from "react";
+import ReactPlayer from "react-player";
+import { OnProgressProps } from "react-player/base";
+
 export default function Page() {
+  const ref = useRef<ReactPlayer | null>();
+  const [playing, setPlaying] = useState(false);
+
+  const [leftBound, rightBound] = [100, 103];
+
+  const seekTo = (seconds: number) => {
+    ref.current?.seekTo(seconds);
+    setPlaying(true);
+  };
+
+  const handleProgress = ({ playedSeconds }: OnProgressProps) => {
+    if (playedSeconds >= leftBound && playedSeconds <= rightBound) {
+      return;
+    }
+
+    seekTo(leftBound);
+  };
+
   return (
-    <section className="prose">
-      <h1>Project</h1>
+    <section className="prose max-w-screen-xl mx-auto pt-5">
+      <h1>Your project</h1>
+
+      <ReactPlayer
+        playing={playing}
+        onPlay={() => setPlaying(true)}
+        ref={(player) => {
+          ref.current = player;
+        }}
+        onProgress={handleProgress}
+        loop={true}
+        controls={true}
+        progressInterval={10}
+        url="https://www.youtube.com/watch?v=LXb3EKWsInQ"
+      />
     </section>
   );
 }
