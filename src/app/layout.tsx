c25ef59bdb5dd72d08ec8ebb4d6565e692ebb353
reversoid <ancient.gekob@gmail.com>
2024-05-16 02:37:10 +0300

    Implement dark theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import NextLink from "next/link";
 import Image from "next/image";
 import logo from "@/assets/logo.svg";
 import { IconAt, IconBrandGithub } from "@tabler/icons-react";
+import { ThemeSwitcher } from "@/shared/theme-switcher";
 
 export const metadata: Metadata = {
   title: "YTLoop",
@@ -22,21 +23,25 @@ export default function RootLayout({
       <body>
         <AppProviders>
           <div className="flex flex-col h-full">
-            <header className="h-16 border-b-1 shrink-0 flex justify-center">
-              <div className="px-2 h-16 grow flex items-center max-w-screen-xl">
-                <NextLink className="w-fit" href={"/"}>
-                  <Image
-                    className="h-10 w-fit aspect-video"
-                    src={logo}
-                    alt="Logo image"
-                  />
-                </NextLink>
+            <header className="h-16 border-b-1 border-foreground-200 shrink-0 flex justify-center">
+              <div className="px-2 h-16 grow flex justify-between items-center max-w-screen-xl">
+                <div className="dark:lg:bg-foreground-600 p-0.5 rounded-lg">
+                  <NextLink className="w-fit" href={"/"}>
+                    <Image
+                      className="h-10 w-fit aspect-video"
+                      src={logo}
+                      alt="Logo image"
+                    />
+                  </NextLink>
+                </div>
+
+                <ThemeSwitcher />
               </div>
             </header>
 
             <main className="flex-grow">{children}</main>
 
-            <footer className="border-t-1 flex justify-center items-center">
+            <footer className="border-t-1 border-foreground-200 flex justify-center items-center">
               <div className="h-16 flex grow justify-between max-w-screen-xl px-2 items-center">
                 <Button
                   as={Link}
--- src/core/app-providers.tsx
+++ src/core/app-providers.tsx
@@ -3,11 +3,15 @@
 import { NextUIProvider } from "@nextui-org/react";
 import { Provider } from "jotai";
 import { FC, PropsWithChildren } from "react";
-
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 export const AppProviders: FC<PropsWithChildren> = ({ children }) => {
   return (
     <Provider>
-      <NextUIProvider>{children}</NextUIProvider>{" "}
+      <NextUIProvider>
+        <NextThemesProvider defaultTheme="light" attribute="class">
+          {children}
+        </NextThemesProvider>
+      </NextUIProvider>
     </Provider>
   );
 };
