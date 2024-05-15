19e71db2f5693a24f7708eeb4a2b635f2ebb9411
reversoid <ancient.gekob@gmail.com>
2024-05-15 09:55:39 +0300

    Fix padding in root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,10 +21,8 @@ export default function Home() {
   return (
     <section className="h-screen">
       <div className="flex items-center justify-center h-full">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold text-center">
-            Loop any YouTube video
-          </h1>
+        <div className="max-w-md px-3">
+          <h1 className="text-5xl font-bold text-center">Loop YouTube video</h1>
           <p className="py-6 text-center">
             Free, beatiful and easy-to-use service for musicians, allowing them
             to loop specific parts of YouTube video.
