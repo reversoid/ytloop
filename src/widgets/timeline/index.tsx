c3cf5e8da7e5794862036eeae370aa00702451ba
reversoid <ancient.gekob@gmail.com>
2024-05-31 18:30:47 +0300

    Remove unnecessary code

--- src/widgets/timeline/index.tsx
+++ src/widgets/timeline/index.tsx
@@ -1,14 +1,13 @@
-import { FC, useEffect, useMemo, useRef, useState } from "react";
-import { VideoLine } from "./ui/video-line";
-import { TimelineLoop } from "./ui/timeline-loop";
-import { useAtom, useAtomValue, useSetAtom } from "jotai";
-import { Loop, projectLoopsAtom } from "@/entities/project/model";
-import { organizeLoopsIntoGrid } from "./utils/organize-loops-into-grid";
-import { UiLoopsAtom } from "./utils/ui-loops-atom";
-import { ValidLoop, isLoopValid } from "@/entities/project/utils/is-loop-valid";
-import { Card, CardBody } from "@nextui-org/react";
-import { loopColorHash } from "@/shared/utils/loop-color-hash";
+import { projectLoopsAtom } from "@/entities/project/model";
+import { isLoopValid } from "@/entities/project/utils/is-loop-valid";
 import { workspaceCurrentLoopAtom } from "@/entities/workspace/model";
+import { loopColorHash } from "@/shared/utils/loop-color-hash";
+import { Card, CardBody } from "@nextui-org/react";
+import { useAtom, useAtomValue } from "jotai";
+import { FC, useMemo } from "react";
+import { TimelineLoop } from "./ui/timeline-loop";
+import { VideoLine } from "./ui/video-line";
+import { organizeLoopsIntoGrid } from "./utils/organize-loops-into-grid";
 
 export const Timeline: FC = () => {
   const videoLength = 226;
@@ -18,44 +17,6 @@ export const Timeline: FC = () => {
   const loopsGrid = useMemo(() => {
     return organizeLoopsIntoGrid(loops ?? []);
   }, [loops]);
-
-  // const uiLoopsData = useAtomValue(UiLoopsAtom);
-
-  // // const rowRef = useRef<HTMLDivElement>(null);
-
-  // // useEffect(() => {
-  // //   if (loops?.length !== uiLoopsData.length || !rowRef.current) {
-  // //     return;
-  // //   }
-
-  // //   const loopDataWithMaxTextLength = uiLoopsData.reduce((prev, value) => {
-  // //     const prevPercents = (prev.loop.to - prev.loop.from) / videoLength;
-
-  // //     const currentPercents = (value.loop.to - value.loop.from) / videoLength;
-
-  // //     const prevValue = prev.textWidth / prevPercents;
-
-  // //     const currentValue = value.textWidth / currentPercents;
-
-  // //     return currentValue > prevValue ? value : prev;
-  // //   }, uiLoopsData[0]);
-
-  // //   const paddingX = 16;
-  // //   const blockWidth = Math.min(
-  // //     loopDataWithMaxTextLength.textWidth + paddingX,
-  // //     200
-  // //   );
-
-  // //   const loop = loopDataWithMaxTextLength.loop;
-
-  // //   const percentsOfVideo = ((loop.to - loop.from) / videoLength) * 100;
-
-  // //   const widthOfOnePercent = blockWidth / percentsOfVideo;
-
-  // //   const timelineWidth = widthOfOnePercent * 100;
-
-  // //   rowRef.current!.style.width = `${timelineWidth}px`;
-  // // }, [loops, uiLoopsData]);
 
   const [currentLoop, setCurrentLoop] = useAtom(workspaceCurrentLoopAtom);
 
--- src/widgets/timeline/ui/video-line.tsx
+++ src/widgets/timeline/ui/video-line.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
 
 export const VideoLine: FC = () => {
-  return <div className="w-full h-1 bg-cyan-500 rounded-md"></div>;
+  return <div className="w-full h-1 bg-primary rounded-md"></div>;
 };
