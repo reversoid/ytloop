eb79043e3b344deb1b949ddd29ef53b3952842f2
reversoid <ancient.gekob@gmail.com>
2024-06-01 03:09:32 +0300

    Some improvements

--- src/entities/workspace/model/index.ts
+++ src/entities/workspace/model/index.ts
@@ -9,6 +9,6 @@ export const workspaceCurrentLoopAtom = atom<Loop | null>(null);
 
 export const workspaceEnabledCountdown = atom<boolean>(false);
 
-export const workspaceCurrentVideoPosition = atom<number | null>(null);
+export const workspaceCurrentVideoPosition = atom<number>(0);
 
 export const workspaceVideoLength = atom<number>(0);
--- src/widgets/timeline/ui/timeline-options.tsx
+++ src/widgets/timeline/ui/timeline-options.tsx
@@ -1,16 +1,55 @@
-import { workspaceEnabledCountdown } from "@/entities/workspace/model";
+import { projectLoopsAtom } from "@/entities/project/model";
+import { ValidLoop, isLoopValid } from "@/entities/project/utils/is-loop-valid";
+import {
+  workspaceCurrentLoopAtom,
+  workspaceCurrentVideoPosition,
+  workspaceEnabledCountdown,
+  workspaceIsPlayingAtom,
+} from "@/entities/workspace/model";
+import { PlayButton } from "@/features/play-button";
 import { Checkbox } from "@nextui-org/react";
-import { useAtom } from "jotai";
+import { useAtom, useAtomValue, useSetAtom } from "jotai";
 
 export const TimelineOptions = () => {
   const [tickBeforeStart, setTickBeforeStart] = useAtom(
     workspaceEnabledCountdown
   );
 
+  const loops = useAtomValue(projectLoopsAtom);
+
+  const [currentLoop, setCurrentLoop] = useAtom(workspaceCurrentLoopAtom);
+
+  const setIsPlaying = useSetAtom(workspaceIsPlayingAtom);
+
+  const currentVideoPosition = useAtomValue(workspaceCurrentVideoPosition);
+
+  const nearestLoop = loops
+    ?.filter(isLoopValid)
+    ?.reduce(
+      (prevLoop, currentLoop) =>
+        Math.abs(prevLoop.from - currentVideoPosition) >
+        Math.abs(currentLoop.from - currentVideoPosition)
+          ? currentLoop
+          : prevLoop,
+      loops.find(isLoopValid) as ValidLoop
+    );
+
   return (
     <>
       <Checkbox isSelected={tickBeforeStart} onValueChange={setTickBeforeStart}>
         Tick before loop starts
+      </Checkbox>
+
+      <Checkbox
+        isSelected={!currentLoop}
+        onValueChange={(continuePlay) => {
+          setCurrentLoop(continuePlay ? null : nearestLoop ?? null);
+          if (!continuePlay) {
+            setIsPlaying(false);
+          }
+        }}
+      >
+        Continue playing after loop ends
       </Checkbox>
     </>
   );
