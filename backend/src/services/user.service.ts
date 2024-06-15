cf78937ce4ee60d29e62d00c99fb11520bc5551b
reversoid <ancient.gekob@gmail.com>
2024-06-15 22:19:57 +0300

    Define better data structure

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -1,9 +1,11 @@
 import { prismaClient } from "../db/index.js";
 
 export class UserService {
-  createUser() {}
-
   async userExists(email: string) {
+    return Boolean(await prismaClient.user.findFirst({ where: { email } }));
+  }
+
+  async createUser(email: string) {
     return Boolean(await prismaClient.user.findFirst({ where: { email } }));
   }
 }
