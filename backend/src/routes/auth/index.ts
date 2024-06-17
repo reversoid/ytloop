ae5173be01d7e9bd43f50d876761443150fe89ed
reversoid <ancient.gekob@gmail.com>
2024-06-18 00:49:03 +0300

    Implement auth validation

--- backend/src/routes/auth/index.ts
+++ backend/src/routes/auth/index.ts
@@ -1,20 +1,40 @@
 import { FastifyPluginAsync } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+
+export const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+export const registerSchema = z.object({
+  email: z.string().email(),
+  username: z.string().min(1),
+  password: z.string().min(8),
+});
 
 const auth: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify
     .withTypeProvider<ZodTypeProvider>()
-    .post("/login", async function (request, reply) {
-      // const authService = fastify.diContainer.resolve("authService");
-      // TODO body validation
-      return true;
-    });
+    .post(
+      "/login",
+      { schema: { body: loginSchema } },
+      async function (request, reply) {
+        // const authService = fastify.diContainer.resolve("authService");
+        // TODO body validation
+        return true;
+      }
+    );
 
-  fastify.post("/register", async function (request, reply) {});
+  fastify
+    .withTypeProvider<ZodTypeProvider>()
+    .post(
+      "/register",
+      { schema: { body: registerSchema } },
+      async function (request, reply) {}
+    );
 
   fastify.post("/logout", async function (request, reply) {});
-
-  fastify.post("/refresh", async function (request, reply) {});
 };
 
 export default auth;
