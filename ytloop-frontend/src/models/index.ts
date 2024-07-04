1b5e78ce4212d5cc212cfc746372bdf0a74f4d88
reversoid <ancient.gekob@gmail.com>
2024-07-05 02:04:23 +0300

    Add index for models

---
+++ ytloop-frontend/src/models/index.ts
@@ -0,0 +1,8 @@
+// TODO would be better with monorepo shared types
+
+export * from "./invite";
+export * from "./loop";
+export * from "./project";
+export * from "./project-code";
+export * from "./project-invite";
+export * from "./user";
